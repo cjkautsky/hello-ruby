159a8821cf8257002b9ad780b58e160e85e47103
cjkautsky <cjkautsky@gmail.com>
2020-02-20 00:52:36 +0000

    Practice Problems

--- practice.rb
+++ practice.rb
@@ -18,6 +18,34 @@ blockchain = [
 # Write code below that returns the number of KelloggCoin that each user has in their 
 # KelloggCoin "wallet".
 
+ben_wallet = 0
+evan_wallet = 0
+nathan_wallet = 0
+
+for transaction in blockchain
+    if transaction[:from_user] == "ben"
+        ben_wallet = ben_wallet - transaction[:amount]
+    elsif transaction[:from_user] == "evan"
+        evan_wallet = evan_wallet - transaction[:amount]
+    elsif transaction[:from_user] == "nathan"
+        nathan_wallet = nathan_wallet - transaction[:amount]
+    end
+    
+    
+    if transaction[:to_user] == "ben"
+        ben_wallet = ben_wallet + transaction[:amount]
+    elsif transaction[:to_user] == "evan"
+        evan_wallet = evan_wallet + transaction[:amount]
+    elsif transaction[:to_user] == "nathan"
+        nathan_wallet = nathan_wallet + transaction[:amount]
+    end
+
+end
+
+puts "Ben's balance is #{ben_wallet}"
+puts "Evans's balance is #{evan_wallet}"
+puts "Nathan's balance is #{nathan_wallet}"
+
 # It should print out:
 # Ben's KelloggCoin balance is 8000
 # Evan's KelloggCoin balance is 10350
